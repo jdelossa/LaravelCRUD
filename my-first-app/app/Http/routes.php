02834cb37ab0b639ad76c8320b7cc1630f584e14
Joanna De Los Santos <jdelossa93@gmail.com>
2015-04-21 17:02:09 -0400

    changes

--- my-first-app/app/Http/routes.php
+++ my-first-app/app/Http/routes.php
@@ -11,11 +11,16 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::get('/', function()
+    {
+        return 'Hi, This is my app!';
+    }
+);
+//Route::get('/', 'WelcomeController@index');
+//
+//Route::get('home', 'HomeController@index');
+//
+//Route::controllers([
+//	'auth' => 'Auth\AuthController',
+//	'password' => 'Auth\PasswordController',
+//]);
